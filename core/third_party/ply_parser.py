6488a47da3cf0e83b1782eea6280cfce2a916efb
rostikL <rostik@freenet.de>
2015-05-17 16:02:43 +0200

    PlyParser 3.6 module added

---
+++ core/third_party/ply_parser.py
@@ -0,0 +1,50 @@
+"""Base class for all parser classes based on PLY
+Most of this class was shamelessly stolen from the examples"""
+
+import os
+
+import core.third_party.ply.lex as lex
+import core.third_party.ply.yacc as yacc
+
+
+class PlyParser(object):
+    """
+    Base class for a lexer/parser that has the rules defined as methods
+    """
+    tokens = ()
+    precedence = ()
+
+
+    def __init__(self, **kw):
+        """Constructs the parser and the lexer"""
+        self.debug = kw.get('debug', 2)
+        self.names = { }
+        try:
+            modname = os.path.split(os.path.splitext(__file__)[0])[1] + "_" + self.__class__.__name__
+        except:
+            modname = "parser"+"_"+self.__class__.__name__
+        self.debugfile = modname + ".dbg"
+        self.tabmodule = modname + "_" + "parsetab"
+
+        # Build the lexer and parser
+        lex.lex(module=self, debug=self.debug)
+        yacc.yacc(module=self,
+                  debug=self.debug,
+                  debugfile=self.debugfile,
+                  tabmodule=self.tabmodule,
+                  check_recursion=self.debug)
+        self.lex=lex
+        self.yacc=yacc
+        
+    def parse(self,content):
+        """Do the actual parsing
+        @param content: String that is to be parsed
+        @return: Result of the parsing"""
+
+        if self.debug:
+            debug=10
+        else:
+            debug=0
+            
+        return yacc.parse(content,debug=debug)
+
